288219aeaf9d0bdaba68fd8a904c7f28a47133b7
Tycoon James Flores <tycoonjamesflores@gmail.com>
2026-04-21 12:17:49 +0800

    Fix database connection config for deployment

--- dbConnection.php
+++ dbConnection.php
@@ -2,7 +2,11 @@
 $databaseHost = 'localhost';
 $databaseName = 'test';
 $databaseUsername = 'root';
-$databasePassword = 'root';
+$databasePassword = 'admin123';
 
 // Open a new connection to the MySQL server
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+
+if (!$mysqli) {
+	die('Database connection failed: ' . mysqli_connect_error());
+}
